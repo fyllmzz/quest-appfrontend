9d5ab3d65723a9b86b8a27c97dd01a75e0005ecd
feyza yılmaz <feyza.yilmaz@cfn.com.tr>
2023-10-19 10:26:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import User from './components/User/User';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Route, Routes,Switch} from "react-router-dom";
+import About from './components/About/About';
+// import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar>
+
+      </Navbar>
+        <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/users/:userId' component={User}></Route>
+        <Route exact path='/about' component={About}></Route>
+        </Switch>
+
+      </BrowserRouter>
+      {/* <BrowserRouter>
+      <Navbar></Navbar>
+      <Switch>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/users/:userId" element={<User/>}></Route>
+      </Switch>
+      </BrowserRouter> */}
+
     </div>
   );
 }
