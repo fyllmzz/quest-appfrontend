88013621d194b6704d285e5a007ba90e5acc40fc
feyza yılmaz <feyza.yilmaz@cfn.com.tr>
2024-01-02 15:14:16 +0300

    authontication register ve login yapıldı

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Home from './components/Home/Home';
 import User from './components/User/User';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route, Routes,Switch} from "react-router-dom";
+import {BrowserRouter, Route,Switch} from "react-router-dom";
 import About from './components/About/About';
+import Auth from "./components/Auth/Auth";
 // import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route exact path='/' component={Home}></Route>
         <Route exact path='/users/:userId' component={User}></Route>
         <Route exact path='/about' component={About}></Route>
+        <Route exact path='/auth' component ={Auth}></Route>
         </Switch>
 
       </BrowserRouter>
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,97 @@
+import React, {useState} from "react";
+import {FormControl, FormHelperText, Input, InputLabel} from "@mui/material";
+import Button from "@mui/material/Button";
+import router from "react-router-dom/es/Router";
+import {toast, ToastContainer} from "react-toastify";
+
+
+function Auth(){
+    const [username,setUsername]=useState("");
+    const [password, setPassword]=useState("");
+
+    const handlePassword = (value) =>{
+        setPassword(value)
+    }
+    const  handleUsername = (value) =>{
+        setUsername(value)
+    }
+    const sendRequest = (path)=>{
+        fetch("/auth/"+path , {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(
+                {
+                    userName:username,
+                    password:password,
+                }),
+        })
+            .then((res)=>res.json())
+            .then((result) => {localStorage.setItem("tokenKey",result.message);
+                // localStorage.setItem("refreshKey",result.refreshToken);
+                localStorage.setItem("currentUser",result.userId);
+                localStorage.setItem("userName",username)})
+            .catch((err)=>console.log(err))
+    }
+
+
+    const handleRegister =()=>{
+        if(username != "" && password != "" ) {
+            sendRequest("register");
+            setUsername("");
+            setPassword("");
+
+        } else {
+
+           alert("Lütfen boş alanları doldurunuz.")
+        }
+
+    }
+    const handleLogin=()=>{
+        if(username != "" && password != "" ) {
+
+            sendRequest("login");
+            setUsername("");
+            setPassword("");
+
+        } else {
+            alert("Lütfen boş alanları doldurunuz.")
+            //toast.error("Lütfen boş alanları doldurunuz.")
+        }
+
+
+    }
+    return(
+        <div style={{ width: '100%', display:"flex",justifyContent:"center" ,textAlign:"center", margin:"auto", marginTop:"80px"}}>
+        <FormControl>
+
+            <InputLabel>Kullanıcı Adı</InputLabel>
+            <Input
+                value={username}
+                onChange={(e)=>handleUsername(e.target.value)}
+            />
+            <InputLabel style={{top:80}}>Şifre</InputLabel>
+            <Input style={{top:40}}
+                   value={password}
+                   onChange={(e) => handlePassword(e.target.value)}
+                   type="password"
+            />
+            <Button variant="contained"
+                    style={{marginTop:60,
+                    background:'linear-gradient(45deg,#2196F3 30%, #21CBF3 90%)',
+                    color:'white'}}
+                    onClick={handleRegister}
+            > Kayıt Ol </Button>
+            <FormHelperText>Hesabın Var Mı? </FormHelperText>
+            <Button variant="contained"
+                    style={{marginTop:10,
+                        background:'linear-gradient(45deg,#2196F3 30%, #21CBF3 90%)',
+                        color:'white'}}
+                    onClick={handleLogin}
+            > Giriş Yap </Button>
+
+        </FormControl>
+</div>
+    )
+}export default Auth;
